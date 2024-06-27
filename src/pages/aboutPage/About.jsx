2d4505a6a777a56bbb5697ad0632ce3134ad4747
nuriddinovv <blackscarp1onoff@gmail.com>
2024-06-27 20:32:33 +0500

    aa

--- src/pages/aboutPage/About.jsx
+++ src/pages/aboutPage/About.jsx
@@ -1,7 +1,70 @@
 import React, { useContext } from "react";
 import { LanguageContext } from "../../context/Context";
+import about from "../../img/about1.jpg";
+import raleb from "../../img/raled.jpg";
 
 export default function About() {
   const { text } = useContext(LanguageContext);
-  return <div>{text.about.text1}</div>;
+  return (
+    <div className="my-4">
+      <div className="container">
+        <h1 className="text-center">{text.about.title}</h1>
+        <div className="row align-items-center my-4">
+          <div className="text col-6">
+            <p>{text.about.text1}</p>
+            <p>{text.about.text2}</p>
+          </div>
+          <div className="col-6">
+            <img
+              style={{ borderRadius: "5px" }}
+              className="w-100  overflow-hidden"
+              src={about}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="my-4">
+          <p>{text.about.text3}</p>
+          <ul>
+            <li style={{ listStyle: "inside" }}>
+              <i>{text.about.list1}</i>
+            </li>
+            <li style={{ listStyle: "inside" }}>
+              <i>{text.about.list2}</i>
+            </li>
+            <li style={{ listStyle: "inside" }}>
+              <i>{text.about.list3}</i>
+            </li>
+            <li style={{ listStyle: "inside" }}>
+              <i>{text.about.list4}</i>
+            </li>
+            <li style={{ listStyle: "inside" }}>
+              <i>{text.about.list5}</i>
+            </li>
+            <li style={{ listStyle: "inside" }}>
+              <i>{text.about.list6}</i>
+            </li>
+          </ul>
+        </div>
+        <div className="raleb row align-items-center">
+          <div className="col-6">
+            <img
+              style={{ borderRadius: "5px" }}
+              className="w-100"
+              src={raleb}
+              alt=""
+            />
+          </div>
+          <div className="col-6">
+            <p>{text.about.raleb}</p>
+            <p>{text.about.text4}</p>
+          </div>
+        </div>
+        <div className="my-4">
+          <p>{text.about.text5}</p>
+          <p>{text.about.text6}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/contactPage/Contact.jsx
+++ src/pages/contactPage/Contact.jsx
@@ -1,4 +1,58 @@
+import { Link } from "react-router-dom";
+import "./contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faPhone,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons"; // Noto'g'ri importlarni to'g'rilash
+import { useContext } from "react";
+import { LanguageContext } from "../../context/Context";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  const { text } = useContext(LanguageContext);
+  return (
+    <div>
+      <div className="container">
+        <h1 className="text-center my-4">{text.contact.h1}</h1>
+        <div className="row justify-content-center align-items-center">
+          <div className="col-5">
+            <div>
+              <div className="locations">
+                <p>
+                  <FontAwesomeIcon icon={faLocationDot} />
+                  <Link to="">
+                    {text.contact.address} {text.contact.address2}
+                  </Link>
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  <Link to={`mailto:${text.contact.mailto}`}>
+                    {text.contact.mail} {text.contact.mailto}
+                  </Link>
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faPhone} />
+                  <Link to={`tel:998951950055`}>
+                    {text.contact.phone} {text.contact.phonenumber}
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-5">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.193415524065!2d69.2393715420055!3d41.2774334869973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8a8a65339a47%3A0x7be3a713adadc47b!2sSafdosh%20St%2C%20Tashkent%2C%20Uzbekistan!5e1!3m2!1sen!2sus!4v1683196047760!5m2!1sen!2sus"
+              width="100%"
+              height="300px"
+              style={{ border: 0, borderRadius: "5px" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
