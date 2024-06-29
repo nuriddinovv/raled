f0c3304f647a1e4b7f31f8b39f4b1de4c72ed598
nuriddinovv <blackscarp1onoff@gmail.com>
2024-06-29 12:38:32 +0500

    a

--- src/pages/booksPage/Books.jsx
+++ src/pages/booksPage/Books.jsx
@@ -1,5 +1,62 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import "./book.css";
+import { Link } from "react-router-dom";
+import { LanguageContext } from "../../context/Context";
 export default function Books() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const { text } = useContext(LanguageContext);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/data")
+      .then((result) => {
+        setData(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div>Books</div>
-  )
+    <div>
+      <div className="container book my-4">
+        <h1 className="text-center">{text.books.title}</h1>
+        <div className="search ">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            className="search-input"
+            placeholder="Search..."
+          />
+          <FontAwesomeIcon
+            style={{ color: "#1e2f97" }}
+            icon={faMagnifyingGlass}
+          />
+        </div>
+        <div className="results">
+          {filteredData.length > 0 ? (
+            filteredData.map((item) => (
+              <div key={item.id} className="card">
+                <div className="card-body">
+                  <h5>{item.name}</h5>
+                  <button>
+                    <Link to={item.link}>View more</Link>
+                  </button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>Natijalar topilmadi</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/sendPage/Send.jsx
+++ src/pages/sendPage/Send.jsx
@@ -1,6 +1,68 @@
+import { useContext } from "react";
+import { LanguageContext } from "../../context/Context";
 
 export default function Send() {
+  const { text } = useContext(LanguageContext);
   return (
-    <div>Send</div>
-  )
+    <div>
+      <div className="container">
+        <h1 className="text-center my-4">{text.send.title}</h1>
+        <p>{text.send.text1}</p>
+        <h1 className="fs-2 p-0 py-2">{text.send.requirements}</h1>
+        <ul>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.reqlist1}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.reqlist2}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.reqlist3}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.reqlist4}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.reqlist5}
+          </li>
+        </ul>
+        <h1 className="fs-2 p-0 py-2">{text.send.sources}</h1>
+        <p>{text.send.sourcestext}</p>
+        <h1 className="fs-2 p-0 py-2">{text.send.sourcesstructure}</h1>
+        <ul>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist1}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist2}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist3}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist4}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist5}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist6}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist7}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist8}
+          </li>
+          <li style={{ listStyle: "inside", fontSize: "18px" }}>
+            {text.send.sourceslist9}
+          </li>
+        </ul>
+        <h1 className="fs-2 p-0 py-2 ">{text.send.addreq}</h1>
+        <p>{text.send.addreqtext}</p>
+        <h1 className="fs-2 p-0 py-2 ">{text.send.edit}</h1>
+        <p>{text.send.edittext}</p>
+      </div>
+    </div>
+  );
 }
